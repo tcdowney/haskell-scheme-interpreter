18816e510d2c82e916133e6067b0acae823e3dd2
Tim Downey <tcdowney>
2012-12-26 23:03:35 -0500

    Added exercise descriptions

--- first-steps/ex1.hs
+++ first-steps/ex1.hs
@@ -1,3 +1,7 @@
+{- Exercise 1:
+Change the program so it reads two arguments from the command line,
+and prints out a message using both of them. -}
+
 module Main where
 import System.Environment
 
--- first-steps/ex2.hs
+++ first-steps/ex2.hs
@@ -1,3 +1,9 @@
+{-Exercise 2:
+Change the program so it performs a simple arithmetic operation on 
+the two arguments and prints out the result. You can use read to 
+convert a string to a number, and show to convert a number back into 
+a string. Play around with different operations. -}
+
 module Main where
 import System.Environment
 
